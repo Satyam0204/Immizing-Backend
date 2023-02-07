3359f9d22f6ba6f6f21f613d0411108cc458cd9c
Satyam0204 <satyampattanaik007@gmail.com>
2023-02-08 03:11:20 +0530

    minor changes

--- immizing_backend/api/views.py
+++ immizing_backend/api/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def home(request):
+    print("home")
+    return HttpResponse('hello')
