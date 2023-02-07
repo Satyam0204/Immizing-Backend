0a246c982b9c0bf14344f04ec3e1f44d77502915
Satyam0204 <satyampattanaik007@gmail.com>
2023-02-08 02:09:16 +0530

    configured postgresql db

--- immizing_backend/immizing_backend/settings.py
+++ immizing_backend/immizing_backend/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = "immizing_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
-}
+# DATABASES = {
+#     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+# }
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'immizingdb',
+        'USER': 'immizinguser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
