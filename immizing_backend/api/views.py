2e4414a4f936683d761fb82b5845a4c4eb54a0d0
Satyam0204 <satyampattanaik007@gmail.com>
2023-02-09 02:00:41 +0530

    shifted the fetching access token function to views

--- immizing_backend/api/views.py
+++ immizing_backend/api/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render, HttpResponse
+import requests
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+
+
+def get_access_token():
+    url="https://api-int.uscis.gov/oauth/accesstoken"
+
+    headers={
+        'content-type': 'application/x-www-form-urlencoded'
+    }
+    payload={
+        'grant_type':'client_credentials',
+        'client_id':env('CLIENT_ID'),
+        'client_secret':env('CLIENT_SECRET')
+    }
+    response = requests.post(url,headers=headers,data=payload)
+    data = response.json()
+    access_token=data['access_token']
+    print(access_token)
 
 def home(request):
     print("home")
+    get_access_token()
     return HttpResponse('hello')
